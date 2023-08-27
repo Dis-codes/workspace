3b8e915dfe49023d06d024e9ae5f3da22edc2f12
Noxyyk <me@noxyyk.com>
2023-08-27 13:25:51 +0200

    add blocks (#8)

---
+++ app/src/lib/blocks/Text/index.ts
@@ -0,0 +1,50 @@
+export default [
+    {
+        kind: "block",
+        type: "text"
+    },
+    {
+        kind: "block",
+        type: "text_join"
+    },
+    {
+        kind: "block",
+        type: "text_append"
+    },
+    {
+        kind: "block",
+        type: "text_length"
+    },
+    {
+        kind: "block",
+        type: "text_isEmpty"
+    },
+    {
+        kind: "block",
+        type: "text_indexOf"
+    },
+    {
+        kind: "block",
+        type: "text_charAt"
+    },
+    {
+        kind: "block",
+        type: "text_getSubstring"
+    },
+    {
+        kind: "block",
+        type: "text_changeCase"
+    },
+    {
+        kind: "block",
+        type: "text_trim"
+    },
+    {
+        kind: "block",
+        type: "text_print"
+    },
+    {
+        kind: "block",
+        type: "text_prompt_ext"
+    }
+];
---
+++ app/src/lib/blocks/logic/index.ts
@@ -0,0 +1,31 @@
+export default
+[
+    {
+        "kind": "block",
+        "type": "controls_if"
+      },
+    {
+        kind: "block",
+        type: "logic_compare"
+    },
+    {
+        kind: "block",
+        type: "logic_operation"
+    },
+    {
+        kind: "block",
+        type: "logic_boolean"
+    },
+    {
+        kind: "block",
+        type: "logic_null"
+    },
+    {
+        kind: "block",
+        type: "logic_ternary"
+    },
+    {
+        kind: "block",
+        type: "logic_negate"
+    },
+]

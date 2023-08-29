d18310dd246fda2ee7394a32890b7306f78c631c
noxyyk <setunskyto@gmail.com>
2023-08-29 18:25:41 +0200

    fixed limenade bug

--- app/src/lib/utils/server.ts
+++ app/src/lib/utils/server.ts
@@ -1,4 +1,4 @@
 export default async function GetUserRoles(discordID: string) {
-    const response = await fetch(`/api/v1/discord/user/roles/?id=${discordID}`)
+    const response = await fetch(`https://www.discodes.xyz/api/v1/discord/user/roles/?id=${discordID}`)
     return (await response.json()).roles
 }

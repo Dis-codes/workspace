f94b447107460ce1743fd8d54afe0475139eabbf
noxyyk <setunskyto@gmail.com>
2023-10-20 12:37:20 +0200

    change of everything

--- app/src/lib/userStore.ts
+++ app/src/lib/userStore.ts
@@ -1,51 +1,11 @@
+import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
-
 export const user = writable(null);
 
-export function storageStore(fileName: string) {
-    const isClient = typeof window !== "undefined";
-
-    const storedValue = isClient ? localStorage.getItem("workspace") : null;
-    const initialValueToUse = storedValue ? JSON.parse(storedValue) : {
-        settings: {
-            botName: "My Bot",
-            botDescription: "My Bot Description",
-            updatedAt: new Date().toISOString(),
-        }
-    };
-
-    if (fileName && fileName.split(".").length > 1) {
-        initialValueToUse[fileName] = initialValueToUse[fileName] || {};
+let generatorStorage = browser ? JSON.parse(window.localStorage.getItem('generator') ?? "{}") : {};
+export const generator = writable(generatorStorage);
+generator.subscribe(value => {
+    if (browser) {
+        window.localStorage.setItem('generator', JSON.stringify(value));
     }
-    else{
-        initialValueToUse[fileName] = initialValueToUse[fileName] || "";
-    }
-
-    const stored = writable(initialValueToUse);
-
-    stored.subscribe(($stored) => {
-        if (isClient) {
-            const filesChanged = Object.keys($stored).some((key) => {
-                if (key === "settings") {
-                  return false;
-                }
-              
-                const storedValue = localStorage.getItem("workspace");
-                const stored = storedValue ? JSON.parse(storedValue) : {};
-                
-                // Compare stringified versions of the objects
-                const storedString = JSON.stringify(stored[key]);
-                const currentString = JSON.stringify($stored[key]);
-                
-                return storedString !== currentString;
-              });
-              
-            if (filesChanged) {
-                $stored.settings.updatedAt = new Date().toISOString();
-                localStorage.setItem("workspace", JSON.stringify($stored));
-              }
-        }
-    });
-
-    return stored;
-}
+});
--- app/src/routes/editor/blockmaker/+page.svelte
+++ app/src/routes/editor/blockmaker/+page.svelte
@@ -1,22 +1,43 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { NavBar, AuthCheck } from "$lib/components/Components";
+    import { NavBar } from "$lib/components/Components";
 
     export let data;
     let { supabase, session } = data;
     $: ({ supabase, session } = data);
-
+    $: {
+        try {
+            generator.set(JSON.parse(block))
+            console.log("set generator", $generator)
+        } catch (e) {
+            console.log(e)
+        }
+        testCode()
+    }
+    import {generator} from "$lib/userStore";
     import Blockly from "blockly/core";
     import En from "blockly/msg/en";
     import { BlocklyTool } from "$lib/utils/blockRegistryTool";
     const BlockRegistryTool = new BlocklyTool();
+    import javascriptGenerator from "$lib/javascript.js";
     import BlocklyComponent from "$lib/components/Blockly.svelte";
     import type { Abstract } from "blockly/core/events/events_abstract";
-    import javascriptGenerator from "$lib/javascript.js";
 
     import { OutputType, BlockShape, InputShape } from '$lib/utils/blockRegistryTool';
-  import { json } from "@sveltejs/kit";
-
+    if (!$generator["func"]){
+        console.log("no generator", $generator)
+        generator.set({
+"func": "myfunction",
+"text": "Hello [ARGUMENT1] World!",
+"inline": true,
+"shape": BlockShape.STATEMENT,
+"arguments": {
+        "ARGUMENT1": {
+            "type": InputShape.VALUE,
+            "check": OutputType.ANY
+            }} 
+    })
+    }
     const DarkTheme = Blockly.Theme.defineTheme("a", {
         name: "true_dark",
         base: Blockly.Themes.Classic,
@@ -83,7 +104,7 @@
         theme: DarkTheme,
         renderer: "zelos",
         collapse: false,
-        disable: false,
+        disable: true,
         maxBlocks: 1,
         trashcan: false,
         horizontalLayout: false,
@@ -103,6 +124,7 @@
         let refreshValue: any = undefined;
         window.onbeforeunload = () => refreshValue;
         // according to blockly, event is type Abstract
+        
         workspace.addChangeListener((event: Abstract) => {
             switch (event.type) {
                 case Blockly.Events.BLOCK_FIELD_INTERMEDIATE_CHANGE:
@@ -111,50 +133,26 @@
                 case Blockly.Events.BLOCK_CHANGE:
                 case Blockly.Events.BLOCK_DRAG:
                 case Blockly.Events.BLOCK_MOVE:
-                    // empty string disables reloading
                     refreshValue = "";
+                    break;
+                default:
+                    console.log(event.type);
                     break;
             }
         });
-        reloadBlock()
     });
     
     // Define variables for selector values
-    let selectedOutputType = "none";
-    let selectedBlockShape = Object.keys(BlockShape)[0];
-    let selectedInputShape = Object.keys(InputShape)[0];
-    let text = "Hello [ARGUMENT1] World!";
-    let inline = true;
-    let arguments_block = text.match(/\[(.*?)\]/g) || []
-    let arguments_data = arguments_block.map((arg) => {
-        return `{
-            type: InputShape.${selectedInputShape},
-            check: OutputType.${selectedOutputType}
-        }`
-    })
-    let functionCode = `return args.ARGUMENT1 + " World!";`
-    let generatedBlock = `{}`
+    let testedCode = undefined
+    let functionCode = `return args.ARGUMENT1 + " World!";` 
+    let block = JSON.stringify($generator, null, 4)
     class Block {
         getRegistry() {
             return {
                 id: "test",
                 color: "%{BKY_VARIABLES_HUE}",
                 blocks: [
-                    {
-                        func: functionName,
-                        text: text,
-                        inline: inline,
-                        output: OutputType[selectedOutputType],
-                        shape: BlockShape[selectedBlockShape],
-                        arguments: {
-                            ARGUMENT1: {
-                                type: InputShape[selectedInputShape]
-                            }
-                            
-                        }
-                        
-                       
-                    }
+                    $generator
                 ],
             };
         }
@@ -163,42 +161,22 @@
         }
     }
     BlockRegistryTool.registerFromBlockset(new Block());
-
-    function GenerateCode() {
-        code = "te"
-        code = javascriptGenerator.workspaceToCode(workspace);
-        console.log(code);
+    //load workspace
+    function testCode() {
+        try {
+            generator.set(JSON.parse(block))
+            let code = `let args = {`
+            for (const [key, value] of Object.entries($generator.arguments)) {
+                code += `${key}: ${value.type == InputShape.VALUE ? "1" : "true"},`
+            }
+            code += `};`
+            code += functionCode
+            let func = new Function(code);
+            testedCode = func();
+        } catch (e) {
+            testedCode = e;
+        }
     }
-    function reloadBlock() {
-        arguments_block = [...new Set(text.match(/\[(.*?)\]/g))] || []
-        arguments_data = arguments_block.map((arg) => {
-        return `{
-        type: InputShape.${selectedInputShape},
-        check: OutputType.${selectedOutputType}
-        }`
-    })
-        generatedBlock = `
-{
-func: "${functionName}",
-text: "${text}",
-inline: ${inline},${selectedOutputType == "none" ? "" : (`\noutput: OutputType.${selectedOutputType},`)}${selectedBlockShape == "none" ? "" : (`\nshape: BlockShape.${selectedBlockShape},`)}
-${arguments_block.length == 0 ? "" : (
-    `arguments: {
-        ${arguments_block.map((arg) => {
-            const argName = arg.slice(1, -1);
-            return `${argName}: {
-            type: InputShape.${selectedInputShape},
-            check: OutputType.${selectedOutputType}
-            }}`
-        }).join(",\n")} 
-    }`
-    )}
-}`
-console.log((generatedBlock))
-        BlockRegistryTool.registerFromBlockset(new Block());
-        workspace.updateToolbox(toolbox);
-    }
-    let code = "e";
 </script>
 
 <NavBar>
@@ -208,72 +186,22 @@ console.log((generatedBlock))
         <div class="w-full h-full">
             <BlocklyComponent {config} locale={en} bind:workspace />
         </div>
-        <div class="w-full">
-            <h3 class="text-2xl font-bold">Selector</h3>
-            <div>
-                <label for="functionName">Function Name:</label>
-                <input type="text" id="functionName" name="functionName" bind:value={functionName} on:change={reloadBlock} />
-            </div>
-            <div>
-                <label for="text">Text:</label>
-                <input type="text" id="text" name="text" bind:value={text} on:change={reloadBlock} />
-            </div>
-            <div>
-                <label for="inline">Inline:</label>
-                <input type="checkbox" id="inline" name="inline" bind:checked={inline} on:change={reloadBlock} />
-            </div>
-            <div>
-                <label for="functionCode">Function Code:</label>
-                <textarea id="functionCode" name="functionCode" bind:value={functionCode} on:change={reloadBlock}/>
-            </div>
-            <div>
-                <label for="outputType">Output Type:</label>
-                <select id="outputType" name="outputType" bind:value=
-                {selectedOutputType} on:change={reloadBlock}>
-                <option value="none">None</option> 
-                    {#each Object.keys(OutputType) as outputType}
-                        <option value={outputType}>{OutputType[outputType]}</option>
-                    {/each}
-                </select>
-            </div>
-            <div>
-                <label for="blockShape">Block Shape:</label>
-                <select id="blockShape" name="blockShape" bind:value={selectedBlockShape} on:change={reloadBlock}>
-                    <option value="none">None</option> 
-                    {#each Object.keys(BlockShape) as blockShape}
-                        <option value={blockShape}>{BlockShape[blockShape]}</option>
-                    {/each}
-                </select>
-            </div>
-            <div>
-                <label for="inputShape">Input Shape:</label>
-                <select id="inputShape" name="inputShape" bind:value={selectedInputShape} on:change={reloadBlock}>
-                    <option value="none">None</option> 
-                    {#each Object.keys(InputShape) as inputShape}
-                        <option value={inputShape}>{inputShape.toLowerCase()}</option>
-                    {/each}
-                </select>
-            </div>
+        <div class="w-full m-6">
             <!-- You can add more selectors as needed -->
             <h3 class="text-2xl font-bold">Block JSON</h3>
             <div>
-                <div>
-                    <pre>
-                        {generatedBlock}
-
-                    </pre>
-                
+                <div class="m-6">
+                    <textarea class="textarea textarea-bordered w-full h-96 resize-none" placeholder="Bio" bind:value={block}></textarea>
                 </div>
-                <h3 class="text-2xl font-bold">Function</h3>
-                <pre>{`${functionName}(args: any) 
-{
+                <h3 class="text-2xl font-bold ">Function</h3>
+                <pre class="m-6 textarea textarea-bordered">{`${$generator["func"]}(args: any){
     ${functionCode}
 }`}</pre>
             </div>
-            <h3 class="text-2xl font-bold">Generated code</h3>
-            <div>
-                <button on:click={GenerateCode}>Generate Code</button>
-                <pre>{code}</pre>
+            <h3 class="text-2xl font-bold">Console</h3>
+            <div class="m-6">
+                <textarea class="textarea textarea-bordered w-full resize-none" placeholder="Bio">{testedCode}</textarea>
+                <button class="btn btn-primary mt-2" on:click={testCode}>Run</button>
             </div>
         </div>
     </div>

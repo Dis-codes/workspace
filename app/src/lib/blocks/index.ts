b84f1728e26d9259db72fdd98a14815482a392bd
noxyyk <setunskyto@gmail.com>
2023-12-23 22:52:27 +0100

    removed old import

---
+++ app/src/lib/blocks/index.ts
@@ -0,0 +1,13 @@
+const importBlocks = async () => {
+    const modules = import.meta.glob('./js/**/**/*.ts');
+  
+    const blocks = [];
+    for (const path in modules) {
+      const module = await modules[path]();
+      blocks.push(module.default || module);
+    }
+  
+    return blocks;
+  };
+  const blocks = await importBlocks();
+  export default blocks;

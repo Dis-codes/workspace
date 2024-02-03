47fc7caaad6378652010779a76a161c87cff3e6f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-03 15:36:14 +0100

    Update index.ts

--- app/src/lib/blocks/js/djs/role/index.ts
+++ app/src/lib/blocks/js/djs/role/index.ts
@@ -1,58 +1,78 @@
 import { OutputType, BlockShape, InputShape } from '$lib/utils/blockRegistryTool';
 class RoleBlocks {
-	getRegistry () {
-		return {
-		  id: "role",
-		  color: '#30B474',
-		  blocks: [
+  getRegistry () {
+    return {
+      id: "role",
+      color: '#30B474',
+      blocks: [
         {
           func: "get",
           text: "Get role with ID [ID] on server [SERVER]",
           output: OutputType.DISCORD.ROLE,
-					inline: true,
-					arguments: {
-						ID: {
-							type: InputShape.VALUE,
-							check: OutputType.STRING
-						},
+          inline: true,
+          arguments: {
+            ID: {
+              type: InputShape.VALUE,
+              check: OutputType.STRING
+            },
             SERVER: {
               type: InputShape.VALUE,
-							check: OutputType.DISCORD.SERVER
+              check: OutputType.DISCORD.SERVER
             }
-					}
+          }
         },
         {
           func: "get_all",
           text: "Get all roles from [VAL] then \n",
           branches: 1,
-					inline: true,
-					arguments: {
-						VAL: {
-							type: InputShape.VALUE,
-							check: OutputType.DISCORD.SERVER.concat(OutputType.DISCORD.MEMBER)
-						}
-					}
+          shape: BlockShape.STATEMENT,
+          inline: true,
+          arguments: {
+            VAL: {
+              type: InputShape.VALUE,
+              check: [OutputType.DISCORD.SERVER, OutputType.DISCORD.MEMBER]
+            }
+          }
         },
         {
           func: "get_all_value",
           text: "Role",
           output: OutputType.DISCORD.ROLE,
-					inline: true,
+          inline: true
+        },
+        {
+          func: "member_has",
+          text: "Member[MEMBER] has role [ROLE]",
+          output: OutputType.BOOLEAN,
+          inline: true,
+          arguments: {
+            MEMBER: {
+              type: InputShape.VALUE,
+              check: OutputType.DISCORD.MEMBER
+            },
+            ROLE: {
+              type: InputShape.VALUE,
+              check: [OutputType.STRING, OutputType.DISCORD.ROLE]
+            }
+          }
         }
-			]
-		};
-	}
-	get(args: any) {
-		return `${args.SERVER}.roles.cache.get(${args.ID})`;
-	}
+      ]
+    };
+  }
+  get(args: any) {
+    return `${args.SERVER}.roles.cache.get(${args.ID})`;
+  }
   get_all(args: any) {
-		return `${args.VAL}.roles.cache.forEach(__DIS__Role => {
+    return `${args.VAL}.roles.cache.forEach(__DIS__Role => {
   ${args.BRANCH1}
 })`;
-	}
+  }
   get_all_value(args: any) {
-		return `__DIS__Role`;
-	}
+    return `__DIS__Role`;
+  }
+  member_has(args: any) {
+    return `${args.MEMBER}.roles.cache.has(${args.ROLE})`
+  }
 }
   
 export default RoleBlocks;
